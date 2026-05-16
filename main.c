f6dd342406bbaa93b526d492b15c04e49866d638
Priyanshu Verma <pvmain1@Priyanshus-MacBook-Air.local>
2026-05-16 19:50:08 +0530

    myshell - initial commit

---
+++ main.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+#define BUFFER_SIZE 1024
+
+void shell_read_loop(void);
+bool shell_read_line(char *, int);
+
+int main(void)
+{
+
+	shell_read_loop();
+	
+	return 0;
+}
+
+
+void shell_read_loop(void){
+
+	char input[BUFFER_SIZE];
+
+	while (1) {	
+
+		fputs("myshell> ", stdout);
+		fflush(stdout);
+		
+
+		if (shell_read_line(input, sizeof(input)) == false)
+			break;
+
+		if (strncmp(input, "exit", 5) == 0) {
+			puts("exiting shell....");
+			break;
+		}
+
+		printf("your command: %s\n", input);
+	}
+
+	puts("exited");
+	
+}
+
+bool shell_read_line(char *input, int size) {
+
+	if (fgets(input, size, stdin) == NULL) {
+		 return false;
+	}
+
+	// newline removal loop
+	int i = 0;
+	while (input[i] != '\0' && input[i] != '\n') {
+		i++;
+	}
+	input[i] = '\0';
+
+	return true;
+}
