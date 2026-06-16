e6e30628d79aa7b788ad83e90989c0fe2987a4ff
Priyanshu-scieck <priyanshu1scieck@gmail.com>
2026-06-16 21:01:25 +0530

    refactor: restructure print.c for single line printing and handle -n flag

--- src/builtins/print.c
+++ src/builtins/print.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "my_shell_builtins.h"
 
 int myshell_print(char **args)
 {
-	for (int i = 1; args[i] != NULL; i++)
-		puts(args[i]);
+	int start = 1;
+	int print_newline = 1;
 
-	if (args[1] == NULL)
-		puts("");
+	if (args[1] && strcmp(args[1], "-n") == 0) {
+		print_newline = 0;
+		start = 2;
+	}
+
+	for (int i = start; args[i]; i++) {
+		if (i != start)
+			fputs(" ", stdout);
+		fputs(args[i], stdout);
+	}
+
+
+	if (print_newline)
+		fputs("\n", stdout);
+	fflush(stdout);
 
 	return 1;
 }
