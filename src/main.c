145a2a2830f1d3473e7f586077b2b8828e1f0e86
Priyanshu-scieck <priyanshu1scieck@gmail.com>
2026-08-12 12:28:03 +0530

    refactor: decouple shell loop and shell input logic from main.c

--- src/main.c
+++ src/main.c
@@ -1,52 +1,8 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include "my_shell_utils.h"
-#include "my_shell_parser.h"
-#include "my_shell_executor.h"
-
-#define BUFFER_SIZE 1024
-
-void shell_read_loop(void);
-bool shell_read_line(char *, int);
+#include "my_shell_loop.h"
 
 int main(void)
 {
 	shell_read_loop();
 
 	return 0;
-}
-
-void shell_read_loop(void)
-{
-	char input[BUFFER_SIZE];
-	char **args;
-	int status = 1;
-
-	while (status) {	
-		print_prompt();
-		if (shell_read_line(input, sizeof(input)) == false)
-			break;	
-
-		args = shell_parse_line(input);
-		if (args == NULL)
-			puts("parsing error");
-		else
-			status = shell_execute(args);
-
-		free(args);
-	}
-}
-
-bool shell_read_line(char *input, int size)
-{
-	if (fgets(input, size, stdin) == NULL)
-		return false;
-
-	/* removes '\n' in input with '\0' using standard func */
-	input[strcspn(input, "\n")] = '\0';
-
-	return true;
 }
