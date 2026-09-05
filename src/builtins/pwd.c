d9564c6d0efc85d2d0211e5f1d1d6d1c5615fa8e
Priyanshu-scieck <priyanshu1scieck@gmail.com>
2026-09-05 18:57:46 +0530

    feat: implement builtin pwd command

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,8 +1,25 @@
 #include "my_shell_builtins.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int myshell_pwd(char **args)
 {
 	(void)args;
 
+	const char *pwd = getenv("PWD");
+	if (pwd) {
+		puts(pwd);
+		return 1;
+	}
+
+	char *cwd = getcwd(NULL, 0);
+	if (!cwd) {
+		perror("myshell: pwd");
+		return 1;
+	}
+
+	puts(cwd);
+	free(cwd);
 	return 1;
 }
